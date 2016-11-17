d3881a9db7f91ceca911b7f93aa2b781af8f949e
josiahdavis <josiah.j.davis@gmail.com>
2016-11-17 09:06:52 -0800

    plot compromise scores

---
+++ mfa/R/plot.R
@@ -0,0 +1,17 @@
+#' plot
+#'
+#' Create summary plots for the result of the multiple factor analysis
+#' @param x - the mfa object
+#' @param color - optional vector of equal length to the rows of the original data
+#' @return a three plots with the projection of each observation onto the first two
+#' extracted components, the partial factor scores, and the variable loadings
+
+plot <- function(x, ...) UseMethod('plot')
+
+plot.mfa <- function(x, color = NULL){
+  plot_compromise(x, color)
+  # plot_partial_factor(x, color)
+  # plot_variable_loadings(x, color)
+}
+
+# plot(mfa1, color = substr(wine$ID, 1, 2))
---
+++ mfa/R/plot_compromise.R
@@ -0,0 +1,59 @@
+#' plot_compromise
+#'
+#' Plot the compromise scores for the first two extracted dimensions
+#' @param x - the mfa object
+#' @param title - chart title
+#' @param color - optional vector of equal length to the rows of the original data
+#' @return a plot with the projection of each observation onto the first two
+#' extracted components
+
+plot_compromise <- function(x, ...) UseMethod('plot_compromise')
+
+plot_compromise.mfa <- function(x, color = NULL, title = 'Compromise'){
+  
+  par(mfrow = c(1, 1))
+  if(!is.null(color)){
+    
+    # Create color scheme for plotting
+    color_pallette <- c('#e31a1c', '#1f78b4', '#33a02c', '#b2df8a'
+                        , '#fb9a99','#a6cee3', '#fdbf6f')
+    
+    color_scheme <- rep(NA, length(color))
+    for(i in 1:length(unique(color))){
+      color_scheme[color_var == unique(color)[i]] <- color_pallette[i]
+    }
+    
+    # Plot the data
+    plot(x$factorScores[,1]
+         , x$factorScores[,2]
+         , pch = 16
+         , cex = 2
+         , main = title
+         , xlab = 'First Factor'
+         , ylab = 'Second Factor'
+         , col = color_scheme)
+    
+    # Create the legend
+    legend (x = min(x$factorScores[,1])
+            , y = max(x$factorScores[,2])
+            , legend = unique(color)
+            , col = unique(color_scheme)
+            , pch = 16)
+  } else {
+    plot(x$factorScores[,1]
+         , x$factorScores[,2]
+         , main = title
+         , xlab = 'First Factor'
+         , ylab = 'Second Factor')
+  }
+  if(!is.null(attr(mfa1, 'ids'))){
+    text(x$factorScores[,1]
+         , x$factorScores[,2]
+         , pos = 1
+         , labels = attr(mfa1, 'ids'))     
+  }
+  
+  abline(v = 0, h = 0)
+}
+
+# plot_compromise(mfa1, substr(wine$ID, 1, 2))
