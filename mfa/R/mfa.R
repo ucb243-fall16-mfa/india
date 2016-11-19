e7ed232261b565dd41e5603163789cd5e054d648
Yoni Ackerman <jonathan.ackerman0@gmail.com>
2016-11-18 20:41:02 -0800

    intial commit of main mfa function.

---
+++ mfa/R/mfa.R
@@ -0,0 +1,77 @@
+#' mfa
+#'
+#' Perform multiple factor analysis
+#' @param data - a numeric data.frame/matrix containing the data
+#' @param sets - a list containing vectors of variable names/positions
+#'               corresponding to each block
+#' @param ncomps - number of components to report
+#' @param weights -
+#' @param center - logical value: should the data should be centered?
+#' @param scale - logical value: should the data be scaled?
+#' @param ids -
+#' @return an mfa object contaning the pieces of the resulting analysis
+mfa <- function(data, sets, ncomps = NULL, weights = NULL,
+                center = TRUE, scale = TRUE, ids = NULL){
+
+    ## keep track of the number of blocks
+    nGroups <- length(sets)
+    
+    ## extract the datasets
+    Xk <- try(lapply(sets, function(g){
+        as.matrix(scale(data[, g], center = center,
+                        scale = scale) / sqrt(length(IDs) - 1))
+    }))
+    if(identical(class(Xk), "try-error")){
+        stop(paste0("please check that arguments",
+                    " scale and center are logical,",
+                    " sets is a list of vectors containing",
+                    " variable names or positions,",
+                    " and that data is a numeric data.frame",
+                    " or matrix."))
+    }
+
+    ## create mass matrix
+    if(is.null(weights)){
+        weights <- rep(1/nGroups, nGroups)
+    } else if(sum(weights) != 1){
+        stop("weights must add to 1")
+    }
+    M <- diag(weights)
+    
+    ## calculate vector of weights
+    a <- unlist(lapply(Xk, function(d){
+        SVD <- svd(d)
+        rep((SVD$d[1])^(-2), dim(d)[2])
+    }))
+
+    ## create weight matrix
+    A <- diag(a)
+
+    ## prepare matrix for GSVD
+    X <- do.call(cbind, Xk)
+    S <- X %*% A %*% t(X)
+
+    ## perform GSVD
+    SVD <- svd(S)
+    P <- sqrt(solve(M)) %*% SVD$u
+    delta <- sqrt(diag(solve(P) %*% S %*% solve(t(P))))
+
+    ## calculate common factor score and loading matrices
+    Q <- t(X) %*% M %*% P %*% solve(diag(delta))
+    F <- S %*% M %*% P %*% solve(diag(delta))
+
+    ## calculate partial factor score matrices
+    Fk <- lapply(1:nGroups, function(i){
+        nGroups * unique(a)[i] * Xk[[i]] %*% Q[sets[[i]],]
+    })
+
+    ## collect results and return list with class "mfa"
+    ret <- list(lambda = delta^2,
+                commonFactorScores = F,
+                partialFactorScores = Fk,
+                Q = Q, P = P)
+    class(ret) <- c("mfa", class(ret))
+    attr(ret, "ncomps") <- ncomps
+    attr(ret, "ids") <- ids
+    ret
+}
