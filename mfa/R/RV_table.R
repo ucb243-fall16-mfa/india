543de6373333e89660bb2625583c688d5d154ae4
fussballball <cantore-dario@web.de>
2016-11-21 17:32:08 -0800

    Update RV_table.R
    Added check_sets() function

--- mfa/R/RV_table.R
+++ mfa/R/RV_table.R
@@ -8,6 +8,9 @@
 #' @export
 #' @examples RV_table(data, sets = list(1:3, 4:5, 6:7))
 RV_table <- function(dataset, sets){
+  #@Everyone: Would you say the sets here have to be mutually exclusive? check_sets function checks for that.
+  check_sets(sets)
+  
   rvMat <- matrix(rep(0, length(sets)^2), length(sets))
   for(i in 1:length(sets)){
     for(j in i:length(sets)){
