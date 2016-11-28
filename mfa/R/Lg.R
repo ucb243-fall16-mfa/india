407524da640332c2a5d5a3c346bb83a87e622584
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2016-11-28 13:08:20 -0800

    added Lg functions, summary_eigenvalues() and updated RV functions and their tests to be more consistent with the rest of the functions.

---
+++ mfa/R/Lg.R
@@ -0,0 +1,10 @@
+#' Lg
+#'
+#' Function to compute the Lg coefficient between two numeric tables
+#' @param table1 - first numeric data.frame/matrix
+#' @param table2 - second numeric data.frame/matrix
+Lg <- function(table1, table2){
+    num <- sum(diag((table1 %*% t(table1)) %*% (table2 %*% t(table2))))
+    denom <- (svd(table1)$d[1])^2 * (svd(table2)$d[1])^2
+    num / denom
+}
--- mfa/R/RV_table.R
+++ mfa/R/RV_table.R
@@ -1,18 +1,22 @@
 #' RV Table function
 #'
 #' Function to calculate the RV coefficient between sets of variables
-#' @param data the original datatable
+#' @param mfa the mfa object
 #' @param sets list of vectors containing the variables between which the RV
 #'             coefficient
 #' will be calculated
 #' @keywords RV similarity measure table set
 #' @export
 #' @examples RV_table(data, sets = list(1:3, 4:5, 6:7))
-RV_table <- function(dataset, sets){
+RV_table <- function(mfa, sets = NULL){
     ## Everyone: Would you say the sets here have to be
     ## mutually exclusive? check_sets function checks for that.
+    check_mfa(mfa)
+    if(is.null(sets)){
+        sets <- attributes(mfa)$sets
+    }
     check_sets(sets)
-    
+    dataset <- mfa$commonFactorScores %*% t(mfa$Q)
     rvMat <- matrix(rep(0, length(sets)^2), length(sets))
     for(i in 1:length(sets)){
         for(j in i:length(sets)){
