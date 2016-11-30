f3e390491f670259467c9eb9134717cd243afe24
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2016-11-29 18:38:25 -0800

    streamlined contribution function tests

--- mfa/tests/testthat/test-check-contribution-funs.R
+++ mfa/tests/testthat/test-check-contribution-funs.R
@@ -1,6 +1,38 @@
-context("check contribution_var_dim function")
+context("check contribution functions")
 
 load("../testMFAobj.rda")
+
+test_that("contribution of obs compute properly", {
+    ## check that params work properly
+    expect_true(check_contribution_params(MFA, 2))
+    expect_error(check_contribution_params(MFA, 13))
+    expect_error(check_contribution_params("MFA", 2))
+
+    ## check the result is correct
+    testStruct <- structure(c(0.089643, 0.061042, 0.054033, 0.115989, 0.175819, 
+                              0.149068, 0.060955, 0.079889, 0.041751, 0.000499,
+                              0.021149, 0.012537, 0.002487, 9.9e-05, 0.019246,
+                              0.002567, 0.001538, 0.001091, 0.003908, 0.015512,
+                              0.012671, 0.053434, 0.024531, 0.000542),
+                            .Dim = c(12L, 2L))
+    
+    expect_true(identical(testStruct, round(contribution_obs_dim(MFA), 6)))
+})
+
+test_that("contribution tables operate properly", {
+    ## check that params work properly
+    expect_true(check_contribution_params(MFA, 2))
+    expect_error(check_contribution_params(MFA, 13))
+    expect_error(check_contribution_params("MFA", 2))
+
+    ## check the result is correct
+    testStruct <- structure(c(0.101133, 0.100058, 0.101051, 0.096417, 0.097523, 
+                              0.10077, 0.102194, 0.095559, 0.10014, 0.105155,
+                              0.095402, 0.068494, 0.15169, 0.048581, 0.063498,
+                              0.104449, 0.224096, 0.134101, 0.052818, 0.056869),
+                            .Dim = c(10L, 2L))
+    expect_true(identical(testStruct, round(contribution_table_dim(MFA), 6)))
+})
 
 test_that("contribution of variables compute properly", {
     ## check that params work properly
