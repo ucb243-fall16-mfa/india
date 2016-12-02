710ea9cab0a3e7fe6b46ed34bb4b30ea006515ba
Yanli Fan <yanlifan@Yanlis-MacBook-Air.local>
2016-12-01 20:17:55 -0800

    added latest version with dim to sandbox

---
+++ sandbox/yanli/latest version with Dim/server.R
@@ -0,0 +1,71 @@
+
+# load the object to the environment
+
+function(input, output, session) {
+  
+  # install the package from github. If installed locally, sometimes there would 
+  # be error using some functions.
+  
+   devtools::install_github("fussballball/stat243FinalProject/mfa", 
+                            force_deps = FALSE)
+
+  
+  # define the output plot depending on the selection of the mfa object and the 
+  # type of plot chosen by the user.
+    output$plot <- renderPlot({
+      inFile <- input$file1
+      
+      if (is.null(inFile)){
+        return(NULL)
+      }
+      
+      # the LoadToEvvironment function could be found in the global.R file in 
+      # shiny app folder.
+      env_fn <- reactiveFileReader(1000, session, inFile$datapath, 
+                                   LoadToEnvironment)
+      env <- env_fn()
+
+      # the content in the environment is the mfa object.
+      object <- ls(env)
+      print(class(object))
+      mfa1 <- get(object[1], env)
+      sets <- as.numeric(length(mfa1$partialFactorScores))
+      print(sets)
+      output$Dim1 <- renderUI({
+        numericInput("Dim1", "Dim 1", 1,min = 1, max = sets, step = 1)
+      })
+      
+      output$Dim2 <- renderUI({
+        numericInput("Dim2", "Dim 2", 2,min = 1, max = sets, step = 1)
+      })
+      
+      newdim <- reactive({
+        return(c(input$Dim1,input$Dim2))
+      })
+      dim <- newdim()
+      
+      # check whether the object is of mfa class and has the right contents.
+      if (check_mfa(mfa1) != TRUE) {
+        helpText("the object in the Rdata file is not mfa object")
+      } else {
+        # Depending on the user's selection, we'll creat the plot by calling the 
+        # corresponding mfa plot function.
+        if(input$select == "Eigenvalues") {
+          barplot(mfa1$lambda,
+                              las = 1,
+                              main="Barplot of Eigenvalues",
+                              ylab="Eigenvalues",
+                              xlab="");
+        }else if (input$select == "Variable loadings") {
+          plot_variable_loadings(mfa1, dim)
+        }else if (input$select == "Common factor scores") {
+          plot_compromise(mfa1, dim)
+        }else if (input$select == "Partial factors scores") {
+          plot_partial_factor(mfa1, dim)
+        }
+      }
+    })
+    
+
+}
+
