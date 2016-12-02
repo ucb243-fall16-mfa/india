d182d1f0114a178945474627b5fc1793558cc88d
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2016-12-01 22:34:25 -0800

    updated shiny app in package

--- mfa/shiny/server.R
+++ mfa/shiny/server.R
@@ -1,22 +1,7 @@
-
-# need to include figure out how to pass the object in the saved file as the 
-# object "mfa1"
-
-# add the tables, check the colors
-
-
-
-# load the object to the environment
-
-
 function(input, output, session) {
   
-  # install the package from github. If installed locally, sometimes there would 
-  # be error using some functions.
-  devtools::install_github("fussballball/stat243FinalProject/mfa", 
-                           force_deps = FALSE)
-  
-
+  # define the output plot depending on the selection of the mfa object and the 
+  # type of plot chosen by the user.
     output$plot <- renderPlot({
       inFile <- input$file1
       
@@ -24,26 +9,37 @@ function(input, output, session) {
         return(NULL)
       }
       
-      print(inFile$datapath)
-      env_fn <- reactiveFileReader(1000, session, inFile$datapath, LoadToEnvironment)
+      # the LoadToEvvironment function could be found in the global.R file in 
+      # shiny app folder.
+      env_fn <- reactiveFileReader(1000, session, inFile$datapath, 
+                                   LoadToEnvironment)
       env <- env_fn()
-      contents <- ls(env)
-      mfa1 <- get(contents[1], env)
 
-      # Depending on input$input_type, we'll creat the plot user demanded.
-      if(input$select == "Eigenvalues"){
-        barplot(mfa1$lambda,
-                            las = 1,
-                            main="Barplot of Eigenvalues",
-                            ylab="Eigenvalues",
-                            xlab="");
-      }else if (input$select == "Variable loadings") {
-        plot_variable_loadings(mfa1)
-      }else if (input$select == "Common factor scores") {
-        plot_compromise(mfa1)
-      }else if (input$select == "Partial factors scores") {
-        plot_partial_factor(mfa1)
+      # the content in the environment is the mfa object.
+      object <- ls(env)
+      mfa1 <- get(object[1], env)
+      sets <- as.numeric(length(mfa1$partialFactorScores))
+
+      ## check whether the object is of mfa class and has the right contents.
+      if (check_mfa(mfa1) != TRUE) {
+        helpText("the object in the Rdata file is not mfa object")
+      } else {
+        ## Depending on the user's selection, we'll creat the plot 
+        ## by calling the corresponding mfa plot function.
+        if(input$select == "Eigenvalues") {
+          barplot(mfa1$lambda,
+                              las = 1,
+                              main="Barplot of Eigenvalues",
+                              ylab="Eigenvalues",
+                              xlab="");
+        } else if (input$select == "Variable loadings") {
+          plot_variable_loadings(mfa1, c(input$Dim1, input$Dim2))
+        } else if (input$select == "Common factor scores") {
+          plot_compromise(mfa1, c(input$Dim1, input$Dim2))
+        } else if (input$select == "Partial factors scores") {
+          plot_partial_factor(mfa1, c(input$Dim1, input$Dim2))
+        }
       }
-      
     })
 }
+
