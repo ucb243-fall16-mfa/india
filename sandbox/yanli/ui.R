b2881e1a182dcbdddfde787d2a6fc187b10888d3
josiahdavis <josiah.j.davis@gmail.com>
2016-12-01 20:07:38 -0800

    adding Yanlis files to sandbox

--- sandbox/yanli/ui.R
+++ sandbox/yanli/ui.R
@@ -1,14 +1,16 @@
 
-library(shiny)
-
 fluidPage(
-  titlePanel("Multiple Factor Analysis Results"),
+  titlePanel(h1("Multiple Factor Analysis Result", 
+                style = "font-family: 'Times New Roman';
+       font-weight: 500; line-height: 1.1; 
+       color: #000000;")),
   
   sidebarLayout(
     sidebarPanel(
-      fileInput('file', 'Please load the saved mfa object from the .Rdata 
-                file:', accept = c("txt/csv", ".csv")),
-      selectInput("select", label = h3("Select the results to plot"), 
+      fileInput('file1', label = h3('Choose Rdata File:'), accept = c(".Rdata")),
+      helpText("Loading the saved mfa object"),
+      
+      selectInput("select", label = h3("Select the Results to Plot"), 
                   c("Eigenvalues", "Common factor scores", 
                     "Partial factors scores", "Variable loadings"))
     ),
