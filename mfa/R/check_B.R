734f59e6fca6384684be929ebaa20d5821db491d
Yoni Ackerman <jonathan.d.ackerman@gmail.com>
2016-12-01 21:06:42 -0800

    bug in check_B found and resolved

--- mfa/R/check_B.R
+++ mfa/R/check_B.R
@@ -4,7 +4,7 @@
 #' @param B the (hopefully) numeric integer to test
 #' @return boolean - does the test pass?
 check_B <- function(B){
-    if(length(B) != 1 | B %% 0 != 1 | B < 1){
+    if(length(B) != 1 | B %% 1 != 0 | B < 1){
         stop("B must be a positive numeric integer")
     }
     TRUE
