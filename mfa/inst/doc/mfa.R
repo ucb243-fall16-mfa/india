d37364cc20ae72a6a093a59fce34691a663f3c1b
josiahdavis <josiah.j.davis@gmail.com>
2016-12-02 10:30:16 -0800

    removed old named vignette and built new one

--- mfa/inst/doc/mfa.R
+++ mfa/inst/doc/mfa.R
@@ -4,26 +4,16 @@ knitr::opts_chunk$set(collapse = TRUE, comment = "#>", fig.width = 6, fig.align 
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  library(devtools)
-#  install("mfa")
-#  library(mfa)
-#  #after loading the library, change the working directory to the directory
-#  #of the package. In most cases the following line should suffice:
-#  setwd("./mfa/")
-#  #execute the following line to knit the vignette
-#  devtools::build_vignettes() # Note: I don't think they should have to build the vignette or change their working directory
+#  devtools::install_github("fussballball/stat243FinalProject/mfa",
+#                           force_deps = FALSE)
 
 ## ------------------------------------------------------------------------
-setwd('/Users/josiahdavis/Documents/LearningDataScience/Berkeley/STAT243/stat243FinalProject')
 library(mfa)
-wine <- read.csv('../project/wines.csv')
-SETS <- list(c(1:6),c(7:12), c(13:18),c(19:23),c(24:29),c(30:34),c(35:38),c(39:44),c(45:49),c(50:53))
-
-#remove the first row because it contains non-numerical values
-data <- wine[,-1]
-mfa1 <- mfa(data = data, sets = SETS )
-
+setwd('/Users/josiahdavis/Documents/LearningDataScience/Berkeley/STAT243/stat243FinalProject')
+data <- read.csv('../project/wines.csv')
+SETS <- list(2:7, 8:13, 14:19, 20:24, 25:30, 31:35, 36:39, 40:45 , 46:50, 51:54)
+mfa1 <- mfa(data = data, sets = SETS)
 mfa1
-
 
 ## ------------------------------------------------------------------------
 par(mar = rep(1, 4))
@@ -65,4 +55,8 @@ Lg(data[,SETS[[1]]], data[,SETS[[2]]])
 ## ------------------------------------------------------------------------
 Lg_table(mfa = mfa1, SETS[1:4])
 
+
+## ------------------------------------------------------------------------
+mfa2 <- bootstrap(mfa1,  B = 1000, seed = 12)
+mfa2
 
