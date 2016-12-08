193210c09c5d66485bee294eefe1d1231e2b67e1
Gaston Sanchez <gaston.stat@gmail.com>
2016-12-08 10:34:09 -0800

    updated examples in documentation of mfa and RV_table

--- mfa/R/RV_table.R
+++ mfa/R/RV_table.R
@@ -6,25 +6,30 @@
 #'             coefficient
 #' will be calculated
 #' @keywords RV similarity measure table set
+#' @examples
+#' data(wine)                                    
+#' i <- grep("V", colnames(wine))                
+#' data <- wine[,i]                              
+#' sets <- list(1:6, 7:12, 13:18, 19:23, 24:29,  
+#'             30:34, 35:38, 39:44, 45:49, 50:53)
+#' MFA <- mfa(data, sets)
+#' RV_table(MFA, sets = list(1:6, 19:23, 24:29))
 #' @export
-#' @examples RV_table(data, sets = list(1:3, 4:5, 6:7))
 RV_table <- function(mfa, sets = NULL){
-    ## Everyone: Would you say the sets here have to be
-    ## mutually exclusive? check_sets function checks for that.
-    check_mfa(mfa)
-    if(is.null(sets)){
-        sets <- attributes(mfa)$sets
+  check_mfa(mfa)
+  if(is.null(sets)){
+    sets <- attributes(mfa)$sets
+  }
+  check_sets(sets)
+  dataset <- mfa$commonFactorScores %*% t(mfa$Q)
+  rvMat <- matrix(rep(0, length(sets)^2), length(sets))
+  for(i in 1:length(sets)){
+    for(j in i:length(sets)){
+      rvMat[i,j] <- RV(dataset[, sets[[i]]], dataset[, sets[[j]]])
+      if(i != j){
+        rvMat[j, i] <- rvMat[i, j]
+      }
     }
-    check_sets(sets)
-    dataset <- mfa$commonFactorScores %*% t(mfa$Q)
-    rvMat <- matrix(rep(0, length(sets)^2), length(sets))
-    for(i in 1:length(sets)){
-        for(j in i:length(sets)){
-            rvMat[i,j] <- RV(dataset[, sets[[i]]], dataset[, sets[[j]]])
-            if(i != j){
-                rvMat[j, i] <- rvMat[i, j]
-            }
-        }
-    }
-    rvMat
+  }
+  rvMat
 }
